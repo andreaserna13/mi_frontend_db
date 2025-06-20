13bd4e24a478d4725994da77f02289895647e369
andrea serna <sernaandre0921@gmail.com>
2025-06-19 22:45:03 -0500

    Frontend listo para producción

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001', // Asegúrate que sea el puerto del backend
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+  ...(mode === 'development'
+    ? {
+        server: {
+          proxy: {
+            '/api': {
+              target: 'http://localhost:3001',
+              changeOrigin: true,
+              secure: false,
+            },
+          },
+        },
+      }
+    : {}),
+}));
