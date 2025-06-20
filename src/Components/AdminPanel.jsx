a69bfdbd4975d06f223c672c3de72357e0f14563
andrea serna <sernaandre0921@gmail.com>
2025-06-20 00:57:13 -0500

    Frontend listo para despliegue

--- src/Components/AdminPanel.jsx
+++ src/Components/AdminPanel.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './reservas.css';
 
+
 const AdminPanel = ({ setLogueado, setTipoUsuario }) => {
   const [section, setSection] = useState(null);
   const navigate = useNavigate();
