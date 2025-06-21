e14bd2b2c7256342e8db299d6b2f9b65d4decf26
andrea serna <sernaandre0921@gmail.com>
2025-06-21 00:14:01 -0500

    Actualización de vite.config.js para conexión con backend local (Ngrok)

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001', // Para desarrollo local con XAMPP
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  build: {
-    outDir: 'dist', // ⚠️ Obligatorio para que Vercel funcione correctamente
-  },
-}));
-
+});
