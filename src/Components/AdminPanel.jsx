17833f6dae1fc43611de3a3d479a325e9d7d8f44
andrea serna <sernaandre0921@gmail.com>
2025-06-23 13:48:45 -0500

    Fix: corregir import de Reservas.css con mayúscula

--- src/Components/AdminPanel.jsx
+++ src/Components/AdminPanel.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './reservas.css';
+import './Reservas.css';
 
 
 const AdminPanel = ({ setLogueado, setTipoUsuario }) => {
