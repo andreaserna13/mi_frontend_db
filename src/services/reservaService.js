068d445cf13e3eb23d79b138c111974adae4f6b2
andrea serna <sernaandre0921@gmail.com>
2026-08-28 18:51:50 -0500

    Corregir URL API de reservas

--- src/services/reservaService.js
+++ src/services/reservaService.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const API_BASE_URL =
     import.meta.env.VITE_API_BASE_URL ||
-    "http://localhost:3001/api";
+   "http://localhost:3001";
 
-const API = `${API_BASE_URL}/reservas`;
+const API = `${API_BASE_URL}/api/reservas`;
 
 export const obtenerReservas = async () => {
     const response = await axios.get(API);
